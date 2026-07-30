fc4780050928c9c91dab8ccba63c8f5f0e941c80
Artemii Savoliuk <introductiontogit@gmail.com>
2026-07-30 19:42:46 +0200

    Read endpoints with 404

--- assignment01/app/main.py
+++ assignment01/app/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+tasks = [
+    {
+        "id": 1,
+        "title": "First task",
+        "done": True
+    },
+    {
+        "id": 2,
+        "title": "Second task",
+        "done": True
+    },
+    {
+        "id": 3,
+        "title": "Third task",
+        "done": False
+    }
+]
 
 @app.get("/")
 def read_root():
@@ -14,3 +31,18 @@ def read_root():
 @app.get("/health")
 def read_status():
     return {"status": "ok"}
+
+@app.get("/tasks")
+def read_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def read_item(task_id: int):
+    for item in tasks:
+        if item["id"] == task_id:
+            return item
+        
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
