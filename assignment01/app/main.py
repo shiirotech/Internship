27ad94f28d93bf54889daf1c2ecd0d0f3b1a173f
Artemii Savoliuk <introductiontogit@gmail.com>
2026-07-30 20:44:47 +0200

    Create with validation

--- assignment01/app/main.py
+++ assignment01/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body
 
 app = FastAPI()
 
@@ -37,12 +37,29 @@ def read_tasks():
     return tasks
 
 @app.get("/tasks/{task_id}")
-def read_item(task_id: int):
-    for item in tasks:
-        if item["id"] == task_id:
-            return item
+def read_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
         
     raise HTTPException(
         status_code=404,
         detail=f"Task {task_id} not found"
+    )
+
+@app.post("/tasks", status_code=201)
+def add_task(data: dict = Body(...)):
+    title = data.get("title")
+    if title and title.strip():
+        tasks.append({
+            "id": tasks[-1]["id"] + 1,
+            "title": title,
+            "done": False
+        })
+
+        return tasks[-1]
+    
+    raise HTTPException(
+        status_code=400,
+        detail="No title has been specified"
     )
