a67e5db5e9e83623b5591f8fe10832ea1b564314
Artemii Savoliuk <introductiontogit@gmail.com>
2026-08-19 12:15:01 +0200

    Change timestamps

--- assignments/db/init.sql
+++ assignments/db/init.sql
@@ -2,12 +2,12 @@ CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     done BOOLEAN NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
 
 INSERT INTO tasks (title, done, created_at, updated_at)
 VALUES
-    ('First task', False, NOW(), NOW()),
-    ('Second task', False, NOW(), NOW()),
-    ('Third task', False, NOW(), NOW());
+    ('First task', False, CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0)),
+    ('Second task', False, CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0)),
+    ('Third task', False, CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0));
